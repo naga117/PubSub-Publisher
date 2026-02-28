994671f4ba469f3c9c6865462be7788c87a8f61b
Naga <nagagogula91@gmail.com>
2026-02-28 00:52:50 -0600

    Add visible app version and native macOS menu actions

--- pubsub_publisher/ui_main.py
+++ pubsub_publisher/ui_main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Dict, List, Optional, Tuple
 
 from PyQt6.QtCore import QSize, QTimer, Qt
+from PyQt6.QtGui import QAction, QKeySequence
 from PyQt6.QtWidgets import (
     QAbstractItemView,
     QCheckBox,
@@ -29,6 +30,7 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
+from . import __version__
 from .config import add_project, load_config, remove_project, save_config
 from .models import LogEntry
 from .validators import normalize_attribute_rows, validate_required_fields
@@ -234,10 +236,12 @@ class MainWindow(QMainWindow):
         settings_layout.addLayout(self._build_auth_panel("settings"))
         settings_layout.addWidget(QLabel("Application Settings"))
         settings_layout.addLayout(self._build_settings_panel())
+        settings_layout.addWidget(self._build_version_label())
         settings_layout.addStretch()
 
         self.tabs.addTab(settings_tab, "Settings")
 
+        self._build_menu_bar()
         self._project_change_timers = {
             "publish": self._create_project_change_timer("publish"),
             "bulk": self._create_project_change_timer("bulk"),
@@ -250,6 +254,105 @@ class MainWindow(QMainWindow):
     def _apply_compact_style(self) -> None:
         # Use native macOS styling for colors and dropdowns.
         self.setStyleSheet("")
+
+    def _build_menu_bar(self) -> None:
+        menu_bar = self.menuBar()
+
+        file_menu = menu_bar.addMenu("File")
+        preferences_action = QAction("Preferences", self)
+        preferences_action.setShortcut(QKeySequence("Meta+,"))
+        preferences_action.setMenuRole(QAction.MenuRole.PreferencesRole)
+        preferences_action.triggered.connect(lambda: self.tabs.setCurrentIndex(2))
+        file_menu.addAction(preferences_action)
+
+        file_menu.addSeparator()
+        quit_action = QAction("Quit PubSub Publisher", self)
+        quit_action.setShortcut(QKeySequence.StandardKey.Quit)
+        quit_action.setMenuRole(QAction.MenuRole.QuitRole)
+        quit_action.triggered.connect(self.close)
+        file_menu.addAction(quit_action)
+
+        edit_menu = menu_bar.addMenu("Edit")
+        cut_action = QAction("Cut", self)
+        cut_action.setShortcut(QKeySequence.StandardKey.Cut)
+        cut_action.triggered.connect(lambda: self._trigger_focus_widget_action("cut"))
+        edit_menu.addAction(cut_action)
+
+        copy_action = QAction("Copy", self)
+        copy_action.setShortcut(QKeySequence.StandardKey.Copy)
+        copy_action.triggered.connect(lambda: self._trigger_focus_widget_action("copy"))
+        edit_menu.addAction(copy_action)
+
+        paste_action = QAction("Paste", self)
+        paste_action.setShortcut(QKeySequence.StandardKey.Paste)
+        paste_action.triggered.connect(lambda: self._trigger_focus_widget_action("paste"))
+        edit_menu.addAction(paste_action)
+
+        edit_menu.addSeparator()
+        select_all_action = QAction("Select All", self)
+        select_all_action.setShortcut(QKeySequence.StandardKey.SelectAll)
+        select_all_action.triggered.connect(lambda: self._trigger_focus_widget_action("selectAll"))
+        edit_menu.addAction(select_all_action)
+
+        view_menu = menu_bar.addMenu("View")
+        publish_tab_action = QAction("Publish Tab", self)
+        publish_tab_action.setShortcut(QKeySequence("Meta+1"))
+        publish_tab_action.triggered.connect(lambda: self.tabs.setCurrentIndex(0))
+        view_menu.addAction(publish_tab_action)
+
+        bulk_tab_action = QAction("Bulk Publish Tab", self)
+        bulk_tab_action.setShortcut(QKeySequence("Meta+2"))
+        bulk_tab_action.triggered.connect(lambda: self.tabs.setCurrentIndex(1))
+        view_menu.addAction(bulk_tab_action)
+
+        settings_tab_action = QAction("Settings Tab", self)
+        settings_tab_action.setShortcut(QKeySequence("Meta+3"))
+        settings_tab_action.triggered.connect(lambda: self.tabs.setCurrentIndex(2))
+        view_menu.addAction(settings_tab_action)
+
+        window_menu = menu_bar.addMenu("Window")
+        minimize_action = QAction("Minimize", self)
+        minimize_action.setShortcut(QKeySequence("Meta+M"))
+        minimize_action.triggered.connect(self.showMinimized)
+        window_menu.addAction(minimize_action)
+
+        bring_to_front_action = QAction("Bring to Front", self)
+        bring_to_front_action.triggered.connect(self._bring_to_front)
+        window_menu.addAction(bring_to_front_action)
+
+        help_menu = menu_bar.addMenu("Help")
+        about_action = QAction("About PubSub Publisher", self)
+        about_action.setMenuRole(QAction.MenuRole.AboutRole)
+        about_action.triggered.connect(self._show_about_dialog)
+        help_menu.addAction(about_action)
+
+    def _build_version_label(self) -> QLabel:
+        label = QLabel(f"Version: {__version__}")
+        label.setStyleSheet("color: #555;")
+        label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        label.setToolTip("Installed application version")
+        return label
+
+    def _show_about_dialog(self) -> None:
+        QMessageBox.about(
+            self,
+            "About PubSub Publisher",
+            f"PubSub Publisher\nVersion {__version__}",
+        )
+
+    def _bring_to_front(self) -> None:
+        if self.isMinimized():
+            self.showNormal()
+        self.raise_()
+        self.activateWindow()
+
+    def _trigger_focus_widget_action(self, method_name: str) -> None:
+        widget = self.focusWidget()
+        if widget is None:
+            return
+        method = getattr(widget, method_name, None)
+        if callable(method):
+            method()
 
     def _build_project_panel(self) -> QGridLayout:
         layout = QGridLayout()
